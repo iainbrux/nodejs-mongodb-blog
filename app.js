b8d738c1eaafb3b097f196ea1c1b88341e40cb26
iainbrux <me@iainbrux.com>
2021-05-27 16:43:33 +0100

    begin movement from html to ejs

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ const express = require('express');
 
 const app = express();
 
+//register view engine
+
+app.set('view engine', 'ejs');
+
 //listen for requests
 
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname })
+    res.render('index');
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname })
+    res.render('about')
 })
 
 //redirects
@@ -25,5 +29,5 @@ app.get('/about-us', (req, res) => {
 //404 page
 
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
+    res.render('404')
 })
