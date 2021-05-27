73727f7d983d5204b18b5d4a029022f417e49857
iainbrux <me@iainbrux.com>
2021-05-27 18:49:36 +0100

    init to mongoDB and complete partials

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 //express app
 
 const app = express();
 
+//authorise mongoDB
+
+const dbURI =
+  "mongodb+srv://iainbrux:yR6SxtjWEPHEDMfw@nodejs-blog.gupeo.mongodb.net/nodejs-blog?retryWrites=true&w=majority";
+(async function connectToDB() {
+  try {
+    await mongoose.connect(dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to mongoDB");
+    app.listen(3000); //Will listen for requests if successfull
+  } catch (err) {
+    console.log(err);
+  }
+})();
+
 //register view engine
 
 app.set("view engine", "ejs");
 
-//listen for requests
+//middleware & static files
 
-app.listen(3000);
+app.use(express.static("public")); //<--- allows use of 'public' directory for styles.css
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const blogs = [
